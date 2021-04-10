355061c5168b8638c67a8b53a46d00be925c732f
Hayoung Jo <57216896+jjoha0@users.noreply.github.com>
2021-04-10 10:47:08 +0900

    Update 10159.cpp

--- algos_assignment/3rd_grade/HayoungJo_jjoha0/week2/10159.cpp
+++ algos_assignment/3rd_grade/HayoungJo_jjoha0/week2/10159.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+ 
+#define INF 10000000 
+
+int n, m;
+int graph[110][110];
+int a, b, c;
+ 
+void floydWarshall() {
+    for (int k = 1; k <= n; k++)            
+        for (int i = 1; i <= n; i++)         // 출발
+            for (int j = 1; j <= n; j++)     // 도착
+                if (graph[i][k] && graph[k][j])        
+                    graph[i][j] = 1;
+}
+int main()
+{
+    cin >> n >> m;
+    
+    for (int i = 0; i < m; i++) 
+    {   
+        int a, b;
+        cin >> a >> b;
+        graph[a][b] = 1;
+    }
+    floydWarshall();
+    for (int i = 1; i <= n; i++) {
+        int cnt = 0;
+        for (int j = 1; j <= n; j++) {
+            if (!graph[i][j] && !graph[j][i])
+                cnt++;
+        }
+        cout << cnt-1 << endl;
+    }
+    return 0;
+}
